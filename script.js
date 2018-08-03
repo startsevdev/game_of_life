5d60e7f289c02d3099784a70db58ac30561215c4
startsevdev <startsevdev@gmail.com>
2018-08-03 15:50:00 +0300

    1

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ var context = canvas.getContext("2d");
 context.strokeStyle = "#82b1ff";
 context.fillStyle = "#82b1ff";
 
-//setInterval(drawRandomGeneration, 1000);
-
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
@@ -23,18 +21,6 @@ function drawCell(x, y) {
   context.fillRect(x * 600 / rows + 20, y * 600 / rows + 20, 600 / rows, 600 / rows);
 };
 
-function drawRandomGeneration() {
-  drawGrid();
-  //context.clearRect(0, 0, canvas.width, canvas.height);
-  var numberOfCells = getRandomInt(rows * rows * 0.25 , rows * rows * 0.75);
-  for (var i = 0; i < numberOfCells; i++){
-    var x = getRandomInt(0, rows);
-    var y = getRandomInt(0, rows);
-    drawCell(x, y);
-  }
-  console.log(numberOfCells + " cells")
-};
-
 function createArray(rows) { //creates a 2 dimensional array of required height
   var arr = [];
   for (var i = 0; i < rows; i++) {
@@ -44,6 +30,7 @@ function createArray(rows) { //creates a 2 dimensional array of required height
 };
 
 function randomGeneration() {
+  var total = 0;
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < rows; j++) {
       var binary = getRandomInt(0, 2);
@@ -53,8 +40,8 @@ function randomGeneration() {
 }
 
 function drawGeneration() {
+  drawGrid()
   var numberOfCells = 0;
-
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < rows; j++) {
       if (grid[i][j] === 1) {
@@ -63,13 +50,59 @@ function drawGeneration() {
       }
     }
   }
-  console.log(numberOfCells);
+  //console.log(numberOfCells + " cells");
+}
+
+function updateGeneration() {
+  for (var i = 1; i < rows - 1; i++) {
+    for (var j = 1; j < rows - 1; j++) {
+      var totalCells = 0;
+      //console.log(grid[0]);
+      totalCells += grid[i-1][j+1];
+      totalCells += grid[i][j + 1];
+      totalCells += grid[i + 1][j + 1];
+      totalCells += grid[i + 1][j];
+      totalCells += grid[i + 1][j - 1];
+      totalCells += grid[i][j - 1];
+      totalCells += grid[i-1][j-1];
+      totalCells += grid[i - 1][j];
+
+      if (grid[i][j] === 0) {
+        switch (totalCells) {
+          case 3:
+            newGrid[i][j] = 1;
+            break;
+          default:
+            newGrid[i][j] = 0;
+        }
+      } else if (grid[i][j] === 1) {
+        switch (totalCells) {
+          case 2:
+          case 3:
+            newGrid[i][j] = 1;
+            break;
+          default:
+          newGrid[i][j] = 0;
+        }
+      }
+    }
+  }
+  grid = newGrid;
+}
+
+function gameLoop() {
+  drawGeneration();
+  updateGeneration();
 }
 
 
 var rows = 50;
 var grid = createArray(rows);
+var newGrid = createArray(rows);;
+var x = 0;
+var y = 0;
 
 randomGeneration();
-drawGrid();
-drawGeneration();
+setInterval(gameLoop, 250);
+//drawGeneration();
+//console.log(grid[-1][-1]);
